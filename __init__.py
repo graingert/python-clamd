a8cb5d52c884d0133f8945578a6fd9c8e9613d13
Thomas Grainger <tagrain@gmail.com>
2013-01-26 22:05:11 +0000

    change to using distutils2

--- __init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from clamd import __version__
-from clamd import *
--- setup.py
+++ setup.py
@@ -1,23 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup
 
-from distutils.core import setup
+setup(
+    setup_requires=['d2to1'],
+    d2to1=True,
 
-# Install : python setup.py install
-# Register : python setup.py register
-
-#  platform = 'Unix',
-#  download_url = 'http://xael.org/norman/python/pyclamd/',
-
-
-setup (name = 'clamd',
-       version = "0.3.0",
-
-       package_dir={'clamd': ''},
-       packages=['clamd'],
-
-       author = 'Alexandre Norman',
-       author_email = 'norman@xael.org',
-       license ='LGPL',
-       keywords="python, clamav, antivirus, scanner, virus, libclamav".split(", "),
-       url = 'https://github.com/graingert/python-clamd/',
-       description = 'Clamd is a python interface to Clamd (Clamav daemon).',
-       long_description = 'Clamd is a python interface to Clamd (Clamav daemon). By using Clamd, you can add virus detection capabilities to your python software in an efficient and easy way. Instead of PyClamav which uses libclamav, clamd may be used by a closed source product.')
+    # This ensures that the MANIFEST.in is read, but it
+    # will become the default in distutils2.
+    include_package_data=True,
+)
